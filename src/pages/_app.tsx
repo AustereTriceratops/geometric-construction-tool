c49627a0ad462de813f3b9522df2b716b012a2fc
AustereTriceratops <gatewaylevi@gmail.com>
2026-09-10 00:34:49 -0700

    Basic UI buttons added (except erase)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,58 @@
-import "@/styles/globals.css";
+import Erase from '@/assets/EraserIcon.svg';
+import "@/pages/app.css";
+
 import { Canvas } from "@react-three/fiber";
 import {useState, useMemo, useEffect, useRef} from 'react';
+import {Create, Straighten, Architecture, Undo, Delete} from '@mui/icons-material';
+import { SvgIcon } from "@mui/material";
+
+function Point() {
+  return (
+    <mesh>
+      <circleGeometry args={[0.05, 10]}/>
+      <meshStandardMaterial color='black'/>
+    </mesh>
+  )
+}
 
 export default function App() {
   return (
-    <div style={{width: '100vw', height: '100vh'}}>
+    <div style={{position: 'relative', width: '100vw', height: '100vh'}}>
       <Canvas>
         <color attach="background" args={['#e8ddcf']}/>
+        <Point/>
       </Canvas>
+
+      <div style={{
+        position: 'absolute',
+        display: 'inline-flex',
+        flexDirection: 'column',
+        width: '48px',
+        top: '5rem',
+        left: '2rem',
+        gap: '1rem'
+      }}>
+        <div className='modeButton'>
+          <Create fontSize='inherit'/>
+        </div>
+        <div className='modeButton'>
+          <Straighten fontSize='inherit'/>
+        </div>
+        <div className='modeButton'>
+          <Architecture fontSize='inherit'/>
+        </div>
+        <div className='modeButton'>
+          <SvgIcon>
+            
+          </SvgIcon>
+        </div>
+        <div className='modeButton'>
+          <Undo fontSize='inherit'/>
+        </div>
+        <div className='modeButton'>
+          <Delete fontSize='inherit'/>
+        </div>
+      </div>
     </div>
   )
 }
