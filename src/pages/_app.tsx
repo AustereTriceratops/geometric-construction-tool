258afb26c2fdc31dd7adfbcb6281eb7a62f20861
AustereTriceratops <gatewaylevi@gmail.com>
2026-09-10 15:27:53 -0700

    placing points w/ the draw tool

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import Erase from '@/assets/eraser.svg';
 import "@/pages/app.css";
 
 import { Canvas } from "@react-three/fiber";
-import { useState, useMemo, useEffect, useRef } from 'react';
+import { useState, useMemo, useEffect, useRef, MouseEvent } from 'react';
 import { Create, Straighten, Architecture, Undo, Delete } from '@mui/icons-material';
 import { SvgIcon } from "@mui/material";
 
@@ -15,6 +15,17 @@ type InputMode = 'draw' | 'straightedge' | 'compass' | 'erase';
 
 export default function App() {
   const [inputMode, setInputMode] = useState<InputMode>(DRAW);
+
+  /// ===== CANVAS =====
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }, []);
+
+  const aspect = useMemo(() => width/height, [width, height]);
 
   /// ===== POINTS =====
   const [points, setPoints] = useState<number[][]>([[1.2, 2], [-2, -0.6]]);
@@ -35,13 +46,15 @@ export default function App() {
     setHistory(history.concat([newPoints]));
   }
   
-  const [history, setHistory] = useState<number[][][]>([]);
+
+  /// ===== HISTORY =====
+  const [history, setHistory] = useState<number[][][]>([points]);
   
   const undo = () => {
     const len = history.length;
-    if (len == 0) return;
+    if (len <= 1) return;
 
-    const lastState = history[len - 1];
+    const lastState = history[len - 2];
 
     setPoints(lastState);
     setHistory(history.slice(0, len - 1));
@@ -52,11 +65,30 @@ export default function App() {
     setHistory(history.concat([[[]]]));
   }
 
-  console.log(points)
+
+  /// ===== EVENTS =====
+  const onClick = (event: MouseEvent<HTMLDivElement>) => {
+    // both in [0, 1]
+    let x = event.clientX/width;
+    let y = event.clientY/height;
+
+    // transform to x in [-aspect, aspect], y in [-1, 1]
+    x = aspect*(2*x - 1);
+    y = 2*y - 1;
+
+    // transform to scene space
+    const scl = 3.82;
+    x = scl*x;
+    y = -scl*y;
+
+    if (inputMode == DRAW) {
+      addPoint(x, y);
+    }
+  }
 
   return (
     <div style={{position: 'relative', width: '100vw', height: '100vh'}}>
-      <Canvas>
+      <Canvas onClick={onClick}>
         <color attach="background" args={['#e8ddcf']}/>
 
         {points.map((p, i) => (
